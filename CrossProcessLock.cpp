254ba19377b121800324cd94ad274b401f05dbc6
Martin Rozenblum <martinroz25@gmail.com>
2024-07-25 16:06:04 +0300

    removed every this-> 'Unnecessary clutter'

--- CrossProcessLock.cpp
+++ CrossProcessLock.cpp
@@ -9,94 +9,94 @@ CrossProcessLock::CrossProcessLock(std::wstring lockName)
     std::wstring sharedMemoryName = lockName + L"_data";
 
     /* get or create write mutex */
-    this->writeMutex = CreateMutexW(NULL, FALSE, writeMutexName.c_str());
-    if (this->writeMutex == NULL)
+    writeMutex = CreateMutexW(NULL, FALSE, writeMutexName.c_str());
+    if (writeMutex == NULL)
         throw std::runtime_error("Failed to create mutex.");
 
     /* get or create read mutex */
-    this->readMutex = CreateMutexW(NULL, TRUE, readMutexName.c_str());
-    if (this->readMutex == NULL)
+    readMutex = CreateMutexW(NULL, TRUE, readMutexName.c_str());
+    if (readMutex == NULL)
     {
-        CloseHandle(this->writeMutex);
+        CloseHandle(writeMutex);
         throw std::runtime_error("Failed to create mutex.");
     }
     mutexStatus = GetLastError();
 
     /* get or create shared data */
-    this->sharedMemory = CreateFileMappingW(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(int), sharedMemoryName.c_str());
-    if (this->sharedMemory == NULL)
+    sharedMemory = CreateFileMappingW(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(int), sharedMemoryName.c_str());
+    if (sharedMemory == NULL)
     {
-        CloseHandle(this->writeMutex);
-        CloseHandle(this->readMutex);
+        CloseHandle(writeMutex);
+        CloseHandle(readMutex);
         throw std::runtime_error("Failed to create shared memory.");
     }
 
-    this->readCounter = (int*)MapViewOfFile(sharedMemory, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(int));
-    if (this->readCounter == NULL)
+    readCounter = (int*)MapViewOfFile(sharedMemory, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(int));
+    if (readCounter == NULL)
     {
-        CloseHandle(this->sharedMemory);
-        CloseHandle(this->writeMutex);
-        CloseHandle(this->readMutex);
+        CloseHandle(sharedMemory);
+        CloseHandle(writeMutex);
+        CloseHandle(readMutex);
         throw std::runtime_error("Failed to map view of shared memory.");
     }
 
     /* if the mutex was first time created, init data */
     if(mutexStatus != ERROR_ALREADY_EXISTS)
-        *this->readCounter = 0;
-    ReleaseMutex(this->readMutex);
+        *readCounter = 0;
+    ReleaseMutex(readMutex);
 }
 
 CrossProcessLock::~CrossProcessLock()
 {
-    CloseHandle(this->writeMutex);     
-    CloseHandle(this->readMutex);
-    CloseHandle(this->sharedMemory);
-    UnmapViewOfFile(this->readCounter);     
+    CloseHandle(writeMutex);     
+    CloseHandle(readMutex);
+    CloseHandle(sharedMemory);
+    UnmapViewOfFile(readCounter);     
 }
 
 DWORD CrossProcessLock::lock(LockType lockType) 
 {
     DWORD result;
-    this->lockType = lockType;
+    lockType = lockType;
 
     if(lockType == LockType::Write)
-        return WaitForSingleObject(this->writeMutex, INFINITE);
+        return WaitForSingleObject(writeMutex, INFINITE);
     
     //on read lock
-    result = WaitForSingleObject(this->readMutex, INFINITE);
+    result = WaitForSingleObject(readMutex, INFINITE);
     if(result == WAIT_FAILED)
         return result;
     
-    (*this->readCounter)++;
-    if(*this->readCounter == 1) //first reader needs to write lock
+    (*readCounter)++;
+    if(*readCounter == 1) //first reader needs to write lock
     {
-        result = WaitForSingleObject(this->writeMutex, INFINITE);
+        result = WaitForSingleObject(writeMutex, INFINITE);
         if(result == WAIT_FAILED)
         {
-            (*this->readCounter)--;
-            ReleaseMutex(this->readMutex);
+            (*readCounter)--;
+            ReleaseMutex(readMutex);
         }
     }
-    return ReleaseMutex(this->readMutex) || result;
+    return ReleaseMutex(readMutex) || result;
 }
 
 DWORD CrossProcessLock::release()
 {
     DWORD result;
 
-    if(this->lockType == LockType::Write)
-        return ReleaseMutex(this->writeMutex);
+    if(lockType == LockType::Write)
+        return ReleaseMutex(writeMutex);
     
-    result = WaitForSingleObject(this->readMutex, INFINITE);
+    result = WaitForSingleObject(readMutex, INFINITE);
     if(result == WAIT_FAILED)
         return result;
 
-    (*this->readCounter)--;
-    if(*this->readCounter == 0)
+    (*readCounter)--;
+    if(*readCounter == 0)
     {
-        result = ReleaseMutex(this->writeMutex);
+        result = ReleaseMutex(writeMutex);
         if(result) //on failed release, restore counter
-            (*this->readCounter)++;
+            (*readCounter)++;
     }
-    return ReleaseMutex(this->readMutex) || result;
+    return ReleaseMutex(readMutex) || result;
 }
